875f35aee9fde83322b9fbb12f18f32bc8d3a686
Abhishek Ranjan <abhishekr700@gmail.com>
2018-01-10 23:08:26 +0530

    Add profile page & change password option
    Signed-off-by: Abhishek Ranjan <abhishekr700@gmail.com>

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 const route = require('express').Router();
+const bcrypt = require('bcryptjs');
+
 const Users = require("../models/sql/sequelize").Users;
 const models = require("../models/mongodb/mongo");
 
@@ -18,13 +20,63 @@ route.get("/all", (req, res) => {
 
 
 //Get User details
-route.get("/details", (req, res) => {
+route.get("/profile", (req, res) => {
     Users.findById(req.user.id, {
         attributes: ["username", "name", "email", "phone1", "phone2"]
     })
         .then((user) => {
-            res.send(user);
+            res.render("profile",{
+                user
+            })
         })
 });
+
+//Render change password page
+route.get("/changePass",(req,res)=>{
+    res.render("change-pass");
+})
+
+//POST route for passord-change
+route.post("/changePass",(req,res)=>{
+    //Check for null values
+    if(req.body.currentpass && req.body.newpass){
+        //Check if old & new password not same
+        if(req.body.currentpass !== req.body.newpass) {
+            Users.findById(req.user.id)
+                .then((user) => {
+                    bcrypt.compare(req.body.currentpass, user.password)
+                        .then((result) => {
+                            if (result) {
+                                bcrypt.genSalt(10, function (err, salt) {
+                                    bcrypt.hash(req.body.newpass, salt, function (err, hash) {
+                                        //Change password in DB
+                                        user.password = hash;
+                                        user.save()
+                                            .then(() => {
+                                                console.log("Pass changed !");
+                                                res.redirect("/users/profile");
+                                            })
+                                            .catch((err) => {
+                                                console.log(err);
+                                            })
+                                    })
+                                })
+
+                            }
+                            else {
+                                res.send("Current Password Incorrect")
+                            }
+                        })
+                })
+        }
+        else{
+            res.send("New password & current password cannot be same");
+        }
+    }
+    //if one of variables null/missing
+    else{
+        res.send("CurrentPass or NewPass not supplied");
+    }
+})
 
 module.exports = route;
