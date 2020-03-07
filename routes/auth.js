815d1d7b64a1176faee4148989218cb56750bd9c
Abhishek Ranjan <abhishekr700@gmail.com>
2020-03-08 01:15:01 +0530

    Update from field in email
    Signed-off-by: Abhishek Ranjan <abhishekr700@gmail.com>

--- routes/auth.js
+++ routes/auth.js
@@ -69,7 +69,7 @@ module.exports = function (app) {
                 });
                 const mailOptions = {
                     to: user.email,
-                    from: CONFIG.SERVER.MAIL,
+                    from: "Auctioneer Team",
                     subject: 'Node.js Password Reset',
                     text: `${'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n'
                     + 'Please click on the following link, or paste this into your browser to complete the process:\n\n'
@@ -120,7 +120,7 @@ module.exports = function (app) {
                 });
                 const mailOptions = {
                     to: user.email,
-                    from: CONFIG.SERVER.MAIL,
+                    from: "Auctioneer Team",
                     subject: 'Your password has been changed',
                     text: `${'Hello,\n\n'
                     + 'This is a confirmation that the password for your account '}${user.email} has just been changed.\n`,
@@ -176,7 +176,7 @@ module.exports = function (app) {
                 });
                 const mailOptions = {
                     to: user.email,
-                    from: CONFIG.SERVER.MAIL,
+                    from: "Auctioneer Team",
                     subject: 'verify email',
                     text: `${'You are receiving this because you (or someone else) have requested for verification of email for your account.\n\n'
                     + 'Please click on the following link, or paste this into your browser to complete the process:\n\n'
